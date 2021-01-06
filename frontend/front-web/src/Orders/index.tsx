469fe023ee819f142395d566ac5bc1ed61e2ed69
Fábio <fdsgoncalves@outlook.com>
2021-01-06 17:27:43 -0300

    backend integration and orders page finished

--- frontend/front-web/src/Orders/index.tsx
+++ frontend/front-web/src/Orders/index.tsx
@@ -1,10 +1,22 @@
 import StepsHeader from './StepsHeader'
-import './style.css'
+import ProductsList from "./ProducstList";
+import { useEffect, useState } from 'react';
+import { Product } from "./Types"
+import { fetchProducts} from "../api";
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetchProducts()
+        .then(Response => setProducts(Response.data))
+        .catch(error => console.log(error) )
+    }, []);
+
     return (
      <div className="orders-container">
          <StepsHeader />
+         <ProductsList products = {products} />
      </div>
     );
   }

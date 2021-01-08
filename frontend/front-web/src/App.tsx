21cccca7be56a635b66a22c36cf8576bbde7b644
Fábio <fdsgoncalves@outlook.com>
2021-01-08 16:05:59 -0300

    Finish frontend

--- frontend/front-web/src/App.tsx
+++ frontend/front-web/src/App.tsx
@@ -1,3 +1,5 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import Routes from './Routers';
 
@@ -5,7 +7,7 @@ function App() {
   return (
     < >
       <Routes />
-
+      <ToastContainer />
     </>
   );
 }
